b984c97ae32382f2be88b4653a87d3b71df909e8
Enriquealf99 <ealfo035@fiu.edu>
2023-04-18 11:32:23 -0400

    Enrique

--- FiuGps/src/Graph/GraphExtension.java
+++ FiuGps/src/Graph/GraphExtension.java
@@ -5,44 +5,40 @@ import java.util.List;
 
 public abstract class GraphExtension implements Graph {
 
-
-	protected  List<Vertex> vertices;
+    protected List<Vertex> vertices;
     protected List<Edge> edges;
 
-    
     GraphExtension(List vertices, List edges) {
         this.vertices = vertices;
         this.edges = edges;
     }
 
-    
     public List vertices() {
         return vertices;
     }
-
 
     public Vertex getVertex(int index) {
         return this.vertices.get(index);
     }
 
-    
     public List edges() {
         return edges;
     }
-    
 
     public List<Edge> incidentEdges(Vertex v) {
-    	
-    	
-    	List<Edge> incidentEdges = new ArrayList<>();
 
-       
-        for (Edge edge : edges) {           
-            if (edge.vertex1.toString().equals(v.toString())) {
+        List<Edge> incidentEdges = new ArrayList<>();
+
+        for (Edge edge : edges) {
+            if (edge.vertex1.toString().equals(v.toString()) || edge.vertex2.toString().equals(v.toString())) {
                 incidentEdges.add(edge);
             }
         }
         return incidentEdges;
+    }
+
+    public List<Vertex> getVertices() {
+        return vertices;
     }
 
 }
---
+++ FiuGps/src/Path/Main.java
@@ -0,0 +1,103 @@
+package Path;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Graph.Edge;
+import Graph.GraphExtension;
+import Graph.GraphImplementation;
+import Graph.Vertex;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		List<Vertex> vertices = new ArrayList<>();
+
+		Vertex v1 = new Vertex("PG6");
+		vertices.add(v1);
+
+		Vertex v2 = new Vertex("PG5");
+		vertices.add(v2);
+
+		Vertex v3 = new Vertex("Graham Center");
+		vertices.add(v3);
+
+		Vertex v4 = new Vertex("PG1");
+		vertices.add(v4);
+
+		Vertex v5 = new Vertex("Blue Parking Grarage (PG2)");
+		vertices.add(v5);
+
+		Vertex v6 = new Vertex("Panther Hall");
+		vertices.add(v6);
+
+		Vertex v7 = new Vertex("Green Library");
+		vertices.add(v7);
+
+		Vertex v8 = new Vertex("University Towers");
+		vertices.add(v8);
+
+		Vertex v9 = new Vertex("Charles E. Perry");
+		vertices.add(v9);
+
+		Vertex v10 = new Vertex("College of Law");
+		vertices.add(v10);
+
+		Vertex v11 = new Vertex("Frost Art Museum");
+		vertices.add(v11);
+
+		Vertex v12 = new Vertex("Parking Garage 3");
+		vertices.add(v12);
+
+		Vertex v13 = new Vertex("Parking Garage 4 (Red Parking Garage)");
+		vertices.add(v13);
+
+		Vertex v14 = new Vertex("college of Business");
+		vertices.add(v14);
+
+		Vertex v15 = new Vertex("Wertheim School of music and preforming arts");
+		vertices.add(v15);
+
+		Vertex v16 = new Vertex("Knight Foundation School of Computing and Information Sciences");
+		vertices.add(v16);
+
+		Vertex v17 = new Vertex("Fiu the Honors College");
+		vertices.add(v17);
+
+		List<Edge> edges = new ArrayList<>();
+
+		edges.add(new Edge(v1, v13, 1260));
+		edges.add(new Edge(v1, v16, 860));
+		edges.add(new Edge(v1, v12, 3705));
+		edges.add(new Edge(v13, v2, 973));
+		edges.add(new Edge(v13, v16, 961));
+		edges.add(new Edge(v16, v7, 1350));
+		edges.add(new Edge(v7, v3, 1015));
+		edges.add(new Edge(v7, v9, 1160));
+		edges.add(new Edge(v7, v17, 916));
+		edges.add(new Edge(v3, v4, 1071));
+		edges.add(new Edge(v3, v11, 1800));
+		edges.add(new Edge(v4, v5, 750));
+		edges.add(new Edge(v5, v15, 1050));
+		edges.add(new Edge(v15, v11, 940));
+		edges.add(new Edge(v11, v5, 650));
+		edges.add(new Edge(v11, v9, 2470));
+		edges.add(new Edge(v3, v9, 805));
+		edges.add(new Edge(v9, v17, 720));
+		edges.add(new Edge(v9, v8, 1190));
+		edges.add(new Edge(v17, v8, 1058));
+		edges.add(new Edge(v8, v6, 1173));
+		edges.add(new Edge(v8, v10, 2136));
+		edges.add(new Edge(v7, v10, 2666));
+		edges.add(new Edge(v10, v12, 1813));
+
+		GraphImplementation gp = new GraphImplementation(vertices, edges);
+
+		Path p = new Path();
+		p.shortestPath(gp, v1, v5);
+
+	}
+}
+
+// method to find the shortest path
